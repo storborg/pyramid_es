8e1312f79291de24daf55eca60ad8b2adab61244
Scott Torborg <scott@cartlogic.com>
2014-11-30 21:50:20 -0800

    Bump version for new release

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = u'2014, Scott Torborg'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.0'
+version = '0.3.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyramid_es',
-      version='0.3.0',
+      version='0.3.1',
       description='Elasticsearch integration for Pyramid.',
       long_description=open('README.rst').read(),
       classifiers=[
